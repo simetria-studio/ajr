9379da3660cda8efe29c860e21c5d175cde73562
Simetria Studio <github@simetria.studio>
2022-12-27 16:20:12 -0300

    atat

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -56,7 +56,7 @@
                         @if (Route::has('login'))
                             <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
                                 @auth
-                                    <a href="{{ url('/home') }}"
+                                    <a href="{{ url('/dashboard') }}"
                                         class="text-sm text-gray-700 dark:text-gray-500 underline">
                                         {{ Auth::user()->name }}</a>
                                 @else
